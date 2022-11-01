03bce61d0dda1011c2bf5559c30d0b67963639d6
Rubens Oliveira <rubensoliveira.prof@gmail.com>
2022-11-01 20:57:42 -0300

    generate password function added, still need some fixes

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
-const passwordOutput = document.querySelector('.password-output');
-const copyPaste = document.querySelector('.copy-paste');
-const numberLength = document.querySelector('.number-length');
-const includeUpper = document.querySelector('.include-uppercase');
-const includeLower = document.querySelector('.include-lowercase');
-const includeNumbers = document.querySelector('.include-numbers');
-const includeSymbols = document.querySelector('.include-symbols');
-const generateBtn = document.querySelector('.generate');
+let passwordOutput = document.querySelector('.password-output');
+let copyPaste = document.querySelector('.copy-paste');
+let characterLength = document.getElementById('characterlength').value;
+let includeUpper = document.querySelector('.include-uppercase');
+let includeLower = document.querySelector('.include-lowercase');
+let includeNumbers = document.querySelector('.include-numbers');
+let includeSymbols = document.querySelector('.include-symbols');
+let generateBtn = document.querySelector('.generate');
+let rangeSlider = document.getElementById('rangeslider');
+
+function generate(){
+    passwordOutput.value = '';
+
+    if (includeUpper.checked == false && includeLower.checked == false && includeNumbers.checked == false && includeSymbols.checked == false){
+        $(includeLower).click();
+    }
+    if (includeLower.checked == true) {
+        var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        lowerLetters.push.apply(lowerLetters);
+    }
+    if (includeUpper.checked == true) {
+        var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        upperLetters.push.apply(upperLetters);
+    }
+    if (includeNumbers.checked == true) {
+        var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+        numbers.push.apply(numbers);
+    }
+    if (includeSymbols.checked == true) {
+        var symbols = ["?", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", "+", "=", "[", "]", "{", "}", ";", ":", "<", ">"];
+        symbols.push.apply(symbols);
+    }
+    for (var i = 0; i < length; i++) {
+        let char = lowerLetters[Math.floor(Math.random() * lowerLetters.characterLength)];
+        passwordOutput.value = passwordOutput.value + char;
+    }
+}
+
+generateBtn.addEventListener('click', function(){
+    generate();
+});
